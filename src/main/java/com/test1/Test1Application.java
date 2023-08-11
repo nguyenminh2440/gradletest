3fabe9a691df7ff126cae0570d7f8f89b3b8ccb0
Minh <nguyenminh2440@gmail.com>
2023-08-11 08:27:06 +0700

    fix

--- src/main/java/com/test1/Test1Application.java
+++ src/main/java/com/test1/Test1Application.java
@@ -2,9 +2,16 @@ package com.test1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Test1Application {
+public class Test1Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Test1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test1Application.class, args);
